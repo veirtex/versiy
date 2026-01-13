a53feb9698020720fe5a8045d526a311815cbcdb
veirtex <veirtex@proton.me>
2026-01-14 01:20:44 +0200

    setting up values for redis configs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,19 +13,22 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":3000"),
 		postgresConfig: postgreSQLConfig{
-			addr:           env.GetString("POSTGRES_ADDR", ""),
-			max_idle_conns: 10, // not used yet
+			addr: env.GetString("POSTGRES_ADDR", ""),
 		},
 		redisConfig: redisConfig{
-			addr:       env.GetString("REDIS_ADDR", ""),
-			pswd:       env.GetString("REDIS_PASSWORD", ""),
-			defualtTTL: time.Duration(time.Hour * 24),
+			addr:         env.GetString("REDIS_ADDR", ""),
+			pswd:         env.GetString("REDIS_PASSWORD", ""),
+			defualtTTL:   time.Duration(time.Hour * 24),
+			dialTimeout:  2 * time.Second,
+			readTimeout:  1 * time.Second,
+			writeTimeout: 1 * time.Second,
+			poolTimeout:  2 * time.Second,
 		},
 		secret:      env.GetString("SECRET", "so secret"),
 		defaultLink: env.GetString("DEFAULT_DOMAIN", ""),
 		rateLimiting: rateLimitConfig{
 			size:     10,
-			duration: time.Duration(time.Second * 10),
+			duration: time.Duration(time.Second * 15),
 		},
 	}
 
@@ -34,9 +37,10 @@ func main() {
 		panic(err)
 	}
 
-	redisClient := database.NewRedisConn(ctx, cfg.redisConfig.addr, cfg.redisConfig.pswd)
+	redisClient := database.NewRedisConn(ctx, cfg.redisConfig.addr, cfg.redisConfig.pswd, cfg.redisConfig.dialTimeout, cfg.redisConfig.readTimeout, cfg.redisConfig.writeTimeout, cfg.redisConfig.poolTimeout)
 
 	defer conn.Close(ctx)
+	defer redisClient.Close()
 
 	app := application{
 		cfg:   cfg,
