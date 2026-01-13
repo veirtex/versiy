5b6870e63652b1caeb453ea2d3a25063baa992eb
veirtex <veirtex@proton.me>
2026-01-14 01:19:44 +0200

    added more configs to redis connection

--- internal/database/connection.go
+++ internal/database/connection.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 	_ "github.com/jackc/pgx/v5/stdlib"
@@ -16,11 +17,15 @@ func NewDBConn(ctx context.Context, addr string) (*pgx.Conn, error) {
 	return conn, nil
 }
 
-func NewRedisConn(ctx context.Context, addr string, psw string) *redis.Client {
+func NewRedisConn(ctx context.Context, addr string, psw string, dialTimeout, readTimeout, writeTimeout, poolTimeout time.Duration) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     addr,
-		Password: psw,
-		DB:       0,
+		Addr:         addr,
+		Password:     psw,
+		DB:           0,
+		DialTimeout:  dialTimeout,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+		PoolTimeout:  poolTimeout,
 	})
 	return rdb
 }
